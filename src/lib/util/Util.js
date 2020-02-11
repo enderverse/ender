7ef62700700641d0be75118f1dab2666822adba9
Tyler <rocket0191@gmail.com>
2020-02-10 19:17:03 -0600

    fix(util): Change `req` function to use `execa` module and throw errors

--- src/lib/util/Util.js
+++ src/lib/util/Util.js
@@ -1,4 +1,4 @@
-const { execSync } = require('child_process');
+const execa = require('execa');
 
 class Util {
 
@@ -6,22 +6,28 @@ class Util {
 		throw new Error('This class may not be initiated with new');
 	}
 
-	static req(module) {
+	static req(mod) {
 		try {
-			require.resolve(module);
+			require.resolve(mod);
 		}
-		// eslint-disable-next-line no-unused-vars
 		catch (error) {
-			execSync(`yarn add ${module}`);
+			if (error.code === 'MODULE_NOT_FOUND') {
+				// eslint-disable-next-line no-unused-vars
+				execa(`yarn add ${mod}`).catch((error) => {
+					throw new Error(`Something went wrong when trying to resolve '${mod}'.`);
+				});
+			}
+			else { throw new Error(`Something went wrong when trying to resolve '${mod}'.`); }
+
 			setImmediate(() => {});
 		}
 
 		try {
-			return require(module);
+			return require(mod);
 		}
 		// eslint-disable-next-line no-unused-vars
 		catch (error) {
-			throw new Error(`Could not include "${module}". Aborted.`);
+			throw new Error(`Could not include "${mod}". Aborted.`);
 		}
 	}
 
